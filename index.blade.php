e56b394663b4b5b6062311b5295c3f364fe8c490
mohanpalanisamysan <143154209+mohanpalanisamysan@users.noreply.github.com>
2023-08-25 19:28:03 +0530

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.dashboard.layouts.app')
+
+@section('content')
+
+<div class="page-content-wrapper p-xxl-4">
+	
+    <!-- Title -->
+    <div class="row">
+        <div class="col-12 mb-4 mb-sm-5">
+            <div class="d-sm-flex justify-content-between align-items-center">
+                <h1 class="h3 mb-2 mb-sm-0">Dashboard</h1>
+                <div class="d-grid"><a href="#" class="btn btn-primary-soft mb-0"><i class="bi bi-plus-lg fa-fw"></i> New Booking</a></div>				
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
